684a75986b013fe1ee5cadac54343a5f971ff541
Emily Miles <ms.emily.j.miles@gmail.com>
2022-05-15 16:40:58 -0400

    Add books files

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+  name: name,
+  age: age,
+  pronouns: pronouns,
+}
+  return character;
+}
+
+function saveReview(review, reviewArray) {
+  if (!reviewArray.includes(review)) {
+    return reviewArray.push(review);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(bookTitle, character, bookGenre) {
+var books = {
+  title: bookTitle,
+  mainCharacter: character,
+  pageCount: calculatePageCount(bookTitle),
+  genre: bookGenre,
+};
+  return books;
+}
+
+function editBook(books) {
+  books.pageCount = books.pageCount * 0.75
+  return books.pageCount;
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
